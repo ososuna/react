51679cfc3919893fe5181bef10482a72664ce1e0
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-03 16:47:20 -0500

    Crear index.js

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const saludo = <h1>Hola Mundo</h1>;
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render( saludo, divRoot );

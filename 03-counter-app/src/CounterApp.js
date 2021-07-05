a31e3cfd18e513a9154ff31dc8062acfe13b4f02
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-05 13:08:38 -0500

    Crear componente CounterApp

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+
+    // handleAdd
+    const handleAdd = ( e ) => {
+        console.log( e );
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { value } </h2>
+
+            <button onClick={ handleAdd }>+1</button>
+        </>
+    );
+
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
+export default CounterApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css'
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp saludo = 'Hola Mundo' />, divRoot );
+ReactDOM.render( <CounterApp value = { 5 } />, divRoot );

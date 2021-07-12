5e72737636b6cac5a58dee6ff93ddff7e11571a1
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-11 19:46:48 -0500

    Update PrimeraApp component Enzyme tests

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,20 +1,49 @@
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+
 import PrimeraApp from "../PrimeraApp";
 
 describe('<PrimeraApp /> tests', () => {
     
-    test('should show the message "Hola Mundo"', () => {
+    // test('should show the message "Hola Mundo"', () => {
+        
+    //     const saludo = 'Hola Mundo';
+        
+    //     const { getByText } = render( <PrimeraApp saludo='Hola Mundo' /> );
+
+    //     expect( getByText( saludo ) ).toBeInTheDocument();
+
+    // })
+
+    test('should show <PrimeraApp /> correctly', () => {
         
         const saludo = 'Hola Mundo';
+        const wrapper = shallow( <PrimeraApp saludo = { saludo } /> );
         
-        const { getByText } = render( <PrimeraApp saludo='Hola Mundo' /> );
+        expect( wrapper ).toMatchSnapshot();
 
-        expect( getByText( saludo ) ).toBeInTheDocument();
-
-    })
+    });
     
+
+    test('should show the props subtitle', () => {
+        
+        const saludo = 'Hola Mundo';
+        const subtitle = 'This is a subtitle';
+        
+        const wrapper = shallow(
+            <PrimeraApp
+                saludo = { saludo }
+                subtitulo = { subtitle }
+            />
+        );
+        
+        const textParagraph = wrapper.find('p').text();
+        
+        expect( textParagraph ).toBe( subtitle );
+
+    });
 
 })
 

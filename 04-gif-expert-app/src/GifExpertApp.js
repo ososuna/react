06c76f781c666494f900baa9d6e2810cbc11ca3e
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-12 10:24:32 -0500

    Add category list to GifExpertApp component

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -4,10 +4,29 @@ import PropTypes from 'prop-types';
 
 const GifExpertApp = () => {
 
+    // const categories = ['Zenitsu', 'Sana Twice', 'Kurapika'];
+
+    const [categories, setCategories] = useState(['Zenitsu', 'Sana Twice', 'Kurapika']);
+
+    const handleAdd = () => {
+        setCategories( ['Hunter x Hunter', ...categories] );
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick = { handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( ( category ) => {
+                        return <li key = { category }>{ category }</li>
+                    })
+                }
+            </ol>
+
         </>
     );
 

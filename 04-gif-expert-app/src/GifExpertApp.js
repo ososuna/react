fbafadc61d87f90f0db939c0e9dee6301bfa5ab7
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-12 10:05:22 -0500

    Init GifExpertApp component

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,16 @@
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const GifExpertApp = () => {
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </>
+    );
+
+}
+
+export default GifExpertApp;
--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -1,9 +1,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import GifExpertApp from './GifExpertApp';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <GifExpertApp />,
   document.getElementById('root')
 );

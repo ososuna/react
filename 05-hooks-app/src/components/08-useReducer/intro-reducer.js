0d9c0c8178ec420c7ae191e152ccb67442586f5d
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-19 18:29:58 -0500

    Create 08-useReducer, Reduce Intro

---
+++ 05-hooks-app/src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,32 @@
+
+const initialState = [{
+    id: 1,
+    todo: 'Buy bread',
+    done: false
+}];
+
+const todoReducer = ( state = initialState, action ) => {
+    
+    if ( action?.type === 'add' ) {
+        return [ ...state, action.payload ];
+    }
+    
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo: 'Buy coffee',
+    done: false
+}
+
+const action = {
+    type: 'add',
+    payload: newTodo
+}
+
+todos = todoReducer( todos, action );
+
+console.log(todos);
--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -1,22 +1,24 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './components/07-tarea-memo/Padre';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './components/07-tarea-memo/Padre';
 
-// import { CallBack } from './components/06-memos/CallBack';
-// import { MemoHook } from './components/06-memos/MemoHook';
-// import { Memorize } from './components/06-memos/Memorize';
-// import { Layout } from './components/05-useLayoutEffect/Layout';
-// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// import { FocusScreen } from './components/04-useRef/FocusScreen';
-// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { HookApp } from './HookApp';
-// import { CounterApp } from './components/01-useState/CounterApp';
-// import { CounterCustomHook } from './components/01-useState/CounterCustomHook';
+// // import { CallBack } from './components/06-memos/CallBack';
+// // import { MemoHook } from './components/06-memos/MemoHook';
+// // import { Memorize } from './components/06-memos/Memorize';
+// // import { Layout } from './components/05-useLayoutEffect/Layout';
+// // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// // import { FocusScreen } from './components/04-useRef/FocusScreen';
+// // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// // import { HookApp } from './HookApp';
+// // import { CounterApp } from './components/01-useState/CounterApp';
+// // import { CounterCustomHook } from './components/01-useState/CounterCustomHook';
 
-ReactDOM.render(
-    <Padre />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Padre />,
+//   document.getElementById('root')
+// );
+
+import './components/08-useReducer/intro-reducer';

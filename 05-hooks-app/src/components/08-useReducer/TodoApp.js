29b15cb5c39da3f5b84da17afd53471f3c8ef36c
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-07-19 19:14:53 -0500

    Add css styles to TodoApp component

--- 05-hooks-app/src/components/08-useReducer/TodoApp.js
+++ 05-hooks-app/src/components/08-useReducer/TodoApp.js
@@ -17,14 +17,49 @@ export const TodoApp = () => {
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({ todos.length })</h1>
             <hr/>
-
-            <ul>
-                <li>Hello</li>
-                <li>World</li>
-                <li>From React</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map( ( todo, index ) => (
+                                <li
+                                    key={ todo.id }
+                                    className="list-group-item"
+                                >
+                                    <p className="text-center">{ index + 1 } . { todo.desc }</p>
+                                    <button
+                                        className="btn btn-danger"
+                                    >
+                                        Delete
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Add TODO</h4>
+                    <hr/>
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Learn..."
+                            autoComplete="off"
+                        />
+                        <div className ="d-grid gap-2">
+                            <button
+                                className="btn btn-outline-primary mt-2"
+                            >
+                                Add
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
